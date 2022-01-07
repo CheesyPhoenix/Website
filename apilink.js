92cb925df37de88a2bfe8cd3d92de21b6c920e15
CheesyPhoenix <sindre.he.moe@gmail.com>
2022-01-07 11:07:54 +0100

    https

--- apilink.js
+++ apilink.js
@@ -1,4 +1,4 @@
-let apiLink = "http://143.47.225.214:8081/";
+let apiLink = "https://143.47.225.214:8081/";
 let debugLink = "https://localhost:8080/";
-let mainApiLink = "http://143.47.225.214:8081/";
+let mainApiLink = "https://143.47.225.214:8081/";
 let activeToken = "none";
